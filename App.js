ed4656af6277a03b5f8c1a64ce6424856888265e
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-20 16:03:40 +0700

    add splash screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // import provider
-import { NativeBaseProvider, extendTheme, Box, Text } from 'native-base';
+import { NativeBaseProvider, extendTheme } from 'native-base';
 
 import AppLoading from 'expo-app-loading';
 // import font expo
@@ -24,25 +24,6 @@ export default function App() {
         normal: 'Poppins_400Regular',
         italic: 'Poppins_400Regular_Italic',
       },
-    },
-  };
-  // setup theme
-  const customColor = {
-    primary: {
-      50: '#E3F2F9',
-      100: '#C5E4F3',
-      200: '#A2D4EC',
-      300: '#7AC1E4',
-      400: '#47A9DA',
-      500: '#0088CC',
-      600: '#007AB8',
-      700: '#006BA1',
-      800: '#005885',
-      900: '#003F5E',
-    },
-    // Redefinig only one shade, rest of the color will remain same.
-    amber: {
-      400: '#d97706',
     },
   };
 
